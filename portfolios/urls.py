d164b9d920ffa8a49aa4a7370a4616dc4fc352fe
Jonnathan <stvns.j@proton.me>
2026-07-08 13:23:44 -0400

    Portfolios urls

---
+++ portfolios/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+
+from portfolios import views
+
+app_name = "portfolios"
+
+urlpatterns = [
+    path(
+        "api/portfolios/<int:portfolio_id>/evolution/",
+        views.portfolio_evolution_api,
+        name="portfolio_evolution_api",
+    ),
+]
